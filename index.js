6563c554284a89763485d3a9b43a3f8b03de9f54
usver <em.92@mail.ru>
2020-01-30 19:15:04 +0200

    asd

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 
 app.listen(8000);
+app.use(express.static('public'));
 
-app.get('/*',function(req, res){
+app.get('/h_*',function(req, res){
     console.log(req);
     res.send(req.url + " asfdgdfgh ");
 })
