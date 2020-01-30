b21b1173236c5b09ef3dce7f38fe53a48c38d8eb
usver <em.92@mail.ru>
2020-01-30 18:45:09 +0200

    asd

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000)
+app.listen(8000);
 
-app.get('/*', function(req, res){
-    console.log("/* post");
-    // var dat = new Date();
-    res.end(req.body);
+app.get('/*',function(req, res){
+    console.log(req);
+    res.send(req.url + " asfdgdfgh ");
 })
+
+// const bodyParser = require('body-parser')
+// var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// app.get('/*', urlencodedParser,function(req, res){
+//     console.log("/* post");
+//     // var dat = new Date();
+//     console.log(req);
+//     res.send(req.url + " asfdgdfgh " + req.headers["user-agent"]);
+// })
+// console.log("/* post");
+// var dat = new Date();
